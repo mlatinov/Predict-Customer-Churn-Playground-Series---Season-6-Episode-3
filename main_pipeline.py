1e5d315149d649c2f405f9405487a0aeaf1dde23
mlatinov <latinovmetodi@gmail.com>
2026-04-05 00:27:19 +0300

    update the main pipeline with the first finished pilot run

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,2 +1,19 @@
 
 #### Main Pipeline to run all experiments ######
+import mlflow
+from experiments.exp_logistic_reg import exp_logistic_reg_features
+
+if __name__ == "__main__":
+    # Mlflow Init 
+    mlflow.set_tracking_uri("sqlite:///mlruns/mlflow.db")
+    mlflow.set_experiment("Kaggle : Churn Prediction")
+
+    # Run Logistic Regression Experiment with Diffrent Feature Engineerings 
+    exp_logistic_reg_features(
+        RUN_Logistic_regression_baseline = True,
+        RUN_DALEX_GLOBAL_EXPLANATIONS = True,
+        RUN_DALEX_LOCAL_EXPLANATIONS = True,
+        LOG_MODEL = False
+    )
+
+
