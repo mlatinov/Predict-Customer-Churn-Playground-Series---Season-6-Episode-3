d0c23961f562ea0a659c44b34a77179f7b7c8a9f
mlatinov <latinovmetodi@gmail.com>
2026-04-05 23:59:06 +0300

    add more preproc experiment and finish the features

--- experiments/exp_logistic_reg.py
+++ experiments/exp_logistic_reg.py
@@ -10,9 +10,12 @@ from functions.modeling_f.dalex_f import (
     dx_local_explanations,
     mlflow_log_dalex_plot
 )
-  
+
 def exp_logistic_reg_features(
-    RUN_Logistic_regression_baseline = True,
+    RUN_Logistic_regression_baseline = False,
+    RUN_Logistic_regression_tenure = False,
+    RUN_Logistic_regression_value_x_service = False,
+    RUN_Logistic_regression_full_feature = False,
     RUN_DALEX_GLOBAL_EXPLANATIONS = True,
     RUN_DALEX_LOCAL_EXPLANATIONS = True,
     LOG_MODEL = False
@@ -24,115 +27,236 @@ def exp_logistic_reg_features(
     model training, evaluation, DALEX explainability, and MLflow logging. Each
     enabled stage is recorded as a separate MLflow run under the 'Logistic_regression
     baseline' run name.
-
-    Returns
-    -------
-    None
-        All outputs (metrics, figures, model artifacts) are logged directly
-        to the active MLflow tracking server.
-
-    Notes
-    -----
-    The DALEX explainer is only instantiated when at least one of
-    RUN_DALEX_GLOBAL_EXPLANATIONS or RUN_DALEX_LOCAL_EXPLANATIONS is True,
-    avoiding unnecessary computation overhead.
     """
-    #=============== Logictic Regression No Features Added =====================================
-    if RUN_Logistic_regression_baseline : 
+    match True:
+        case _ if RUN_Logistic_regression_baseline:
+            model_to_run = "Logistic Regressioon Baseline"
+            feature_eng = "None"
+            features_names = ["tenure","MonthlyCharges","TotalCharges"]
+        case _ if RUN_Logistic_regression_tenure:
+            model_to_run = "Logistic Regression Tenure"
+            feature_eng = "Tenure"
+            features_names = ["tenure","MonthlyCharges","TotalCharges"]
+        case _ if RUN_Logistic_regression_value_x_service:
+            model_to_run = "Logistic Regreesion Value & Service"
+            feature_eng = "Value & Service"
+            features_names = ["tenure","MonthlyCharges","TotalCharges","value_gap"]
+        case _ if RUN_Logistic_regression_full_feature:
+            model_to_run = "Logistic Regression Full Eng"
+            feature_eng = "Full"
+            features_names = ["tenure","MonthlyCharges","TotalCharges","value_gap"]
+        case _:
+            model_to_run = None
+            
+    # =============== Logictic Regression Features Experiment =====================================
+    with mlflow.start_run(run_name = model_to_run) :
 
-        with mlflow.start_run(run_name = "Logistic_regression baseline") :
+        # Experimental Settings 
+        params = {
+            "model"           : "Logistic Regreesion",
+            "model_type"      : "Linear Models",
+            "model_params"    : "Defaults",
+            "train_data_size" :  0.7,
+            "test_data_size"  :  0.3,
+            "preprocessing"   : "Scale + One_Hot",
+            "feature_eng"     : feature_eng  
+        }
+        mlflow.log_params(params)
 
-            # Experimental Settings 
-            params = {
-                "model"           : "Logistic Regreesion",
-                "model_type"      : "Linear Models",
-                "model_params"    : "Defaults",
-                "train_data_size" :  0.7,
-                "test_data_size"  :  0.3,
-                "preprocessing"   : "Scale + One_Hot",
-                "feature_eng"     : "None"  
-            }
-            mlflow.log_params(params)
-
-            # Split the data 
-            model_data = data_split(
+        # Split the data 
+        model_data = data_split(
                 train_url= "sample_data/train.csv",
                 train_size= 0.7,
                 test_size = 0.3
+        )
+
+        # Build a model pipeline only with the original features 
+        pipeline = build_pipeline(
+            model= LogisticRegression(class_weight="balanced"),
+            add_tenure= RUN_Logistic_regression_tenure,
+            add_value_x_service= RUN_Logistic_regression_value_x_service,
+            add_full_feature= RUN_Logistic_regression_full_feature
+        ) 
+        # Fit the pipeline on the training data 
+        pipeline.fit(
+            X = model_data["x_train"],
+            y = model_data["y_train"]
+        )
+
+        # Predict with the model 
+        model_predictions = model_predict(pipeline, model_data)
+
+        # Evaluate the model and plot Confusion Matrix 
+        eval = evaluate_model(
+            model= pipeline,
+            predictions=model_predictions,
+            model_data= model_data
+        )
+        conf_matrix = confusion_matrix(model_predictions, model_data)
+
+        # Log Model Evaluations 
+        mlflow.log_figure(conf_matrix, "plots/confusion_matrix.png")
+        mlflow.log_metrics(eval)
+
+        # Create a DALEX Explainer if Global or Local Explanations = True
+        if RUN_DALEX_GLOBAL_EXPLANATIONS or RUN_DALEX_LOCAL_EXPLANATIONS :
+            dalex_exp = dx_create_explainer(
+                pipeline= pipeline,
+                x_train= model_data["x_train"],
+                y_train= model_data["y_train"],
+                label= model_to_run
             )
-            # Build a model pipeline only with the original features 
-            pipeline = build_pipeline(
-                model= LogisticRegression(),
-                add_tenure= False,
-                add_value_x_service=False,
-                add_full_feature=False 
-            ) 
-            # Fit the pipeline on the training data 
-            pipeline.fit(
-                X = model_data["x_train"],
-                y = model_data["y_train"]
+
+        # ======== Global Explations =============
+        if RUN_DALEX_GLOBAL_EXPLANATIONS :
+            # Run Dalex Global Explanations 
+            gfi = dx_global_importance(
+                dalex_explainer = dalex_exp,
+                features_names = features_names
             )
-            # Predict with the model 
-            model_predictions = model_predict(pipeline, model_data)
+            # Log Global Explanations Plots
+            mlflow_log_dalex_plot(gfi["Loss_Shuffle_Plot"],"variable_importance.html",  "dalex/global")
+            mlflow_log_dalex_plot(gfi["PDP_Plot"]         ,"partial_dependence.html",   "dalex/global")
+            mlflow_log_dalex_plot(gfi["RCDR_Plot"]        ,"residuals_rcdf.html",       "dalex/global")
 
-            # Evaluate the model and plot Confusion Matrix 
-            eval = evaluate_model(
-                model= pipeline,
-                predictions=model_predictions,
-                model_data= model_data
+        # ======== Local Explations =============
+        if RUN_DALEX_LOCAL_EXPLANATIONS :
+            # Run Dalex Local Explanations  
+            lme = dx_local_explanations(
+                pipeline = pipeline,
+                dalex_explainer = dalex_exp,
+                x_train = model_data["x_train"],
+                y_train = model_data["y_train"],
+                features_names = features_names
             )
-            conf_matrix = confusion_matrix(model_predictions, model_data)
+            # Log Local Explanations Plots 
+            mlflow_log_dalex_plot(lme["bd_plot_1"]  ,"breakdown_churn.html",      "dalex/local")
+            mlflow_log_dalex_plot(lme["bd_plot_2"]  ,"breakdown_no_churn.html",   "dalex/local")
+            mlflow_log_dalex_plot(lme["cp_plot_1"]  ,"ceretis_paribus_churn.html",      "dalex/local")
+            mlflow_log_dalex_plot(lme["cp_plot_2"]  ,"ceretis_paribus_no_churn.html",   "dalex/local")
 
-            # Log Model Evaluations 
-            mlflow.log_figure(conf_matrix, "plots/confusion_matrix.png")
-            mlflow.log_metrics(eval)
+        # ============= Logging the model ====================
+        if LOG_MODEL :
+            mlflow.sklearn.log_model(
+                sk_model = pipeline,
+                artifact_path = "model",
+                input_sample = model_data["x_train"].head(5),
+                registered_model_name = model_to_run
+            )
 
-            # Create a DALEX Explainer if Global or Local Explanations = True
-            if RUN_DALEX_GLOBAL_EXPLANATIONS or RUN_DALEX_LOCAL_EXPLANATIONS :
-                dalex_exp = dx_create_explainer(
-                    pipeline= pipeline,
-                    x_train= model_data["x_train"],
-                    y_train= model_data["y_train"],
-                    label= "Logistic Regression Baseline"
-                )
+def exp_logistic_reg_preproc(
+    RUN_Logistic_regression_full_feature = False,
+    RUN_Logistic_regression_baseline = False,
+    RUN_DALEX_GLOBAL_EXPLANATIONS = True,
+    RUN_DALEX_LOCAL_EXPLANATIONS = True,
+    LOG_MODEL = False
+    ) : 
 
-            # ======== Global Explations =============
-            if RUN_DALEX_GLOBAL_EXPLANATIONS :
-                # Run Dalex Global Explanations 
-                gfi = dx_global_importance(
-                    dalex_explainer = dalex_exp,
-                    features_names = ["tenure","MonthlyCharges","TotalCharges"]
-                )
-                # Log Global Explanations Plots
-                mlflow_log_dalex_plot(gfi["Loss_Shuffle_Plot"],"variable_importance.html",  "dalex/global")
-                mlflow_log_dalex_plot(gfi["PDP_Plot"]         ,"partial_dependence.html",   "dalex/global")
-                mlflow_log_dalex_plot(gfi["RCDR_Plot"]        ,"residuals_rcdf.html",       "dalex/global")
+    match True :
+        case _ if RUN_Logistic_regression_full_feature :
+            model_to_run = "Logistic Regression Full Proc"
+            feature_eng = "Full"
+            preproc = "NZV + Yeo + Scale + One-Hot"
+            features_names = ["tenure","MonthlyCharges","TotalCharges","value_gap"]
+        case _ if RUN_Logistic_regression_baseline : 
+            model_to_run = "Logistic Regression Baseline Proc"
+            feature_eng = "None"
+            preproc = "NZV + Yeo + Scale + One-Hot"
+            features_names = ["tenure","MonthlyCharges","TotalCharges"]
+        case _ :
+            model_to_run = None
+    
+    with mlflow.start_run(run_name= model_to_run) : 
 
-             # ======== Local Explations =============
-            if RUN_DALEX_LOCAL_EXPLANATIONS :
-                # Run Dalex Local Explanations  
-                lme = dx_local_explanations(
-                    pipeline = pipeline,
-                    dalex_explainer = dalex_exp,
-                    x_train = model_data["x_train"],
-                    y_train = model_data["y_train"],
-                    features_names = ["tenure","MonthlyCharges","TotalCharges"]
-                )
-                # Log Local Explanations Plots 
-                mlflow_log_dalex_plot(lme["bd_plot_1"]  ,"breakdown_churn.html",      "dalex/local")
-                mlflow_log_dalex_plot(lme["bd_plot_2"]  ,"breakdown_no_churn.html",   "dalex/local")
-                mlflow_log_dalex_plot(lme["cp_plot_1"]  ,"ceretis_paribus_churn.html",      "dalex/local")
-                mlflow_log_dalex_plot(lme["cp_plot_2"]  ,"ceretis_paribus_no_churn.html",   "dalex/local")
+        # Experimental Settings 
+        params = {
+            "model"           : "Logistic Regreesion",
+            "model_type"      : "Linear Models",
+            "model_params"    : "Defaults",
+            "train_data_size" :  0.7,
+            "test_data_size"  :  0.3,
+            "preprocessing"   : preproc,
+            "feature_eng"     : feature_eng  
+        }
+        mlflow.log_params(params)
 
-            # ============= Logging the model ====================
-            if LOG_MODEL :
-                mlflow.sklearn.log_model(
-                    sk_model = pipeline,
-                    artifact_path = "model",
-                    input_sample = model_data["x_train"].head(5),
-                    registered_model_name = "churn_logistic_baseline"
-                )
+        # Split the data 
+        model_data = data_split(
+            train_url= "sample_data/train.csv",
+            train_size= 0.7,
+            test_size= 0.3
+        )
+        # Build a model Pipeline 
+        pipeline = build_pipeline(
+            model = LogisticRegression(),
+            add_remove_nzv= True,
+            add_power_transform = True,
+            add_tenure= False,
+            add_value_x_service=False,
+            add_full_feature= RUN_Logistic_regression_full_feature,
+        )
+        # Fit the pipeline on the training data 
+        pipeline.fit(
+            X = model_data["x_train"],
+            y = model_data["y_train"]
+        )
+        # Predict with the model 
+        model_predictions = model_predict(pipeline, model_data)
 
+        # Evaluate the model and plot Confusion Matrix 
+        eval = evaluate_model(
+            model= pipeline,
+            predictions=model_predictions,
+            model_data= model_data
+        )
+        conf_matrix = confusion_matrix(model_predictions, model_data)
 
+        # Log Model Evaluations 
+        mlflow.log_figure(conf_matrix, "plots/confusion_matrix.png")
+        mlflow.log_metrics(eval)
 
+        # Create a DALEX Explainer if Global or Local Explanations = True
+        if RUN_DALEX_GLOBAL_EXPLANATIONS or RUN_DALEX_LOCAL_EXPLANATIONS :
+            dalex_exp = dx_create_explainer(
+                pipeline= pipeline,
+                x_train= model_data["x_train"],
+                y_train= model_data["y_train"],
+                label= model_to_run
+            )
 
+        # ======== Global Explations =============
+        if RUN_DALEX_GLOBAL_EXPLANATIONS :
+            # Run Dalex Global Explanations 
+            gfi = dx_global_importance(
+                dalex_explainer = dalex_exp,
+                features_names = features_names
+            )
+            # Log Global Explanations Plots
+            mlflow_log_dalex_plot(gfi["Loss_Shuffle_Plot"],"variable_importance.html",  "dalex/global")
+            mlflow_log_dalex_plot(gfi["PDP_Plot"]         ,"partial_dependence.html",   "dalex/global")
+            mlflow_log_dalex_plot(gfi["RCDR_Plot"]        ,"residuals_rcdf.html",       "dalex/global")
+
+        # ======== Local Explations =============
+        if RUN_DALEX_LOCAL_EXPLANATIONS :
+            # Run Dalex Local Explanations  
+            lme = dx_local_explanations(
+                pipeline = pipeline,
+                dalex_explainer = dalex_exp,
+                x_train = model_data["x_train"],
+                y_train = model_data["y_train"],
+                features_names = features_names
+            )
+            # Log Local Explanations Plots 
+            mlflow_log_dalex_plot(lme["bd_plot_1"]  ,"breakdown_churn.html",      "dalex/local")
+            mlflow_log_dalex_plot(lme["bd_plot_2"]  ,"breakdown_no_churn.html",   "dalex/local")
+            mlflow_log_dalex_plot(lme["cp_plot_1"]  ,"ceretis_paribus_churn.html",      "dalex/local")
+            mlflow_log_dalex_plot(lme["cp_plot_2"]  ,"ceretis_paribus_no_churn.html",   "dalex/local")
+
+        # ============= Logging the model ====================
+        if LOG_MODEL :
+            mlflow.sklearn.log_model(
+                sk_model = pipeline,
+                artifact_path = "model",
+                input_sample = model_data["x_train"].head(5),
+                registered_model_name = model_to_run
+            )
