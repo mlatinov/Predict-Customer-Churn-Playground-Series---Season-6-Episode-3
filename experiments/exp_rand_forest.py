31a24b90d82f425e4fe9bc4e06e2860b9f35b631
mlatinov <latinovmetodi@gmail.com>
2026-04-06 23:57:20 +0300

    finish random forest experiment

--- experiments/exp_rand_forest.py
+++ experiments/exp_rand_forest.py
@@ -13,11 +13,16 @@ from functions.modeling_f.dalex_f import (
     mlflow_log_dalex_plot,
     dx_transform
 )
-import pandas as pd 
-def exp_random_forest_tune(RUN_tune_random_forest = True) :
 
-    with mlflow.start_run(run_name="Random Forest Test") : 
-        
+def exp_random_forest_tune(
+    RUN_tune_random_forest = False,
+    RUN_DALEX_GLOBAL_EXPLANATIONS = True,
+    RUN_DALEX_LOCAL_EXPLANATIONS = True,
+    LOG_MODEL = False
+    ) :
+
+    with mlflow.start_run(run_name="Tuned Random Forest ") : 
+
         # Split the data 
         model_data = data_split(
             train_url  ="sample_data/train.csv",
@@ -25,22 +30,44 @@ def exp_random_forest_tune(RUN_tune_random_forest = True) :
             test_size  = 0.3
         )
 
-         # Build Model Pipeline 
+        # Build Model Pipeline with 
+        # Already Tuned parameters Have to Run Tune again if data changes 
         pipeline = build_pipeline(
-            model               = RandomForestClassifier(),
+            model = RandomForestClassifier(
+                max_depth             = 29,
+                max_features          = 0.6972,
+                n_estimators          = 809,
+                min_impurity_decrease = 0.0003,
+                min_samples_leaf      = 2,
+                min_samples_split     = 3 
+            ),
             add_tenure          =False,
             add_value_x_service =False,
             add_full_feature    =True,
             add_power_transform =True,
             add_remove_nzv      =True
         )
-        
         # Fit the pipeline 
         pipeline.fit(
             X = model_data["x_train"],
             y = model_data["y_train"]
         )
+        # Run predictions with the model 
+        model_predictions = model_predict(model = pipeline, model_data = model_data)
+
+        # Evaluate the model 
+        eval = evaluate_model(
+            model       = pipeline,
+            predictions = model_predictions,
+            model_data  = model_data
+        )
+        # Create a Confusion Matirx 
+        conf_matrix = confusion_matrix(predictions= model_predictions, model_data = model_data)
+        mlflow.log_metrics(eval)
+        mlflow.log_figure(conf_matrix, "plots/confusion_matrix.png")
+
         # ======== Random grid Search ==============
+        # Run Only if the data Changes The best parameters are already in the model 
         if RUN_tune_random_forest :
 
             # Declare a parameter Grid 
@@ -65,5 +92,70 @@ def exp_random_forest_tune(RUN_tune_random_forest = True) :
                 X_transformed   = model_data["x_train"],
                 y_train_encoded = tunner_df["y_encoded"],
                 iters = 50
-            )
-            best_params = search.best_params_
+            )            
+            # Refit the pipeline with the best parameters 
+            pipeline = search.best_estimator_
+
+    if RUN_DALEX_GLOBAL_EXPLANATIONS or RUN_DALEX_LOCAL_EXPLANATIONS : 
+        # Create a Dalex Explainer
+        dx_explainer = dx_create_explainer(
+            pipeline = pipeline,
+            x_train  = model_data["x_train"],
+            y_train  = model_data["y_train"],
+            label    = "Tuned Random Forest Model "
+        )
+        features_names  = ["tenure","MonthlyCharges","TotalCharges","value_gap"]
+
+    # ============= Global DALEX Explanations ===================
+    if RUN_DALEX_GLOBAL_EXPLANATIONS : 
+        gfi = dx_global_importance(
+            dalex_explainer = dx_explainer,
+            features_names  = features_names
+        )
+        # Log Global Explanations Plots
+        mlflow_log_dalex_plot(gfi["Loss_Shuffle_Plot"],"variable_importance.html",  "dalex/global")
+        mlflow_log_dalex_plot(gfi["PDP_Plot"]         ,"partial_dependence.html",   "dalex/global")
+        mlflow_log_dalex_plot(gfi["RCDR_Plot"]        ,"residuals_rcdf.html",       "dalex/global")
+
+    # =============== Local DALEX Explanations =================
+    if RUN_DALEX_LOCAL_EXPLANATIONS : 
+        lme = dx_local_explanations(
+            dalex_explainer = dx_explainer,
+            pipeline        = pipeline,
+            features_names  = features_names
+        )
+        # Log Local Explanations Plots 
+        mlflow_log_dalex_plot(lme["bd_plot_1"]  ,"breakdown_churn.html",      "dalex/local")
+        mlflow_log_dalex_plot(lme["bd_plot_2"]  ,"breakdown_no_churn.html",   "dalex/local")
+        mlflow_log_dalex_plot(lme["cp_plot_1"]  ,"ceretis_paribus_churn.html",      "dalex/local")
+        mlflow_log_dalex_plot(lme["cp_plot_2"]  ,"ceretis_paribus_no_churn.html",   "dalex/local")
+
+    # ============ Log The model ==================
+    if LOG_MODEL : 
+        mlflow.sklearn.log_model(
+            sk_model = pipeline,
+            artifact_path = "model",
+            input_sample = model_data["x_train"].head(5),
+            registered_model_name = "Tuned Random Forest Model"
+        )
+
+    # ============ Experimental Settings and Loging  ===============
+    exp_params = {
+        "model"           : "Random Forest",
+        "model_type"      : "Ensemble Tree",
+        "model_params"    : "Tuned",
+        "train_data_size" :  0.7,
+        "test_data_size"  :  0.3,
+        "preprocessing"   : "Yeo + NZV + Scale + One_Hot",
+        "feature_eng"     : "Full"
+    }
+    mlflow.log_params(exp_params)
+    model_params = {
+            "max_depth"             : 29,
+            "max_features"          : 0.6972,
+            "n_estimators"          : 809,
+            "min_impurity_decrease" : 0.0003,
+            "min_samples_leaf"      : 2,
+            "min_samples_split"     : 3 
+    }
+    mlflow.log_model_params(model_params)
