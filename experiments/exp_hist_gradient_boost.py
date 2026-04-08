e46267a6dfefd8364ac3c8785369a51bb350fca5
mlatinov <latinovmetodi@gmail.com>
2026-04-08 21:55:28 +0300

    finish the hist boost experiment

--- experiments/exp_hist_gradient_boost.py
+++ experiments/exp_hist_gradient_boost.py
@@ -1,3 +1,4 @@
+
 import mlflow 
 from sklearn.ensemble import HistGradientBoostingClassifier
 from scipy.stats import randint, uniform
@@ -17,7 +18,7 @@ def exp_hist_gradient_tune(
     RUN_hist_gradient_boost = False,
     RUN_DALEX_GLOBAL_EXPLANATIONS = True,
     RUN_DALEX_LOCAL_EXPLANATIONS = True,
-    LOG_MODEL = False
+    RUN_LOG_MODEL = False
     ) : 
     with mlflow.start_run(run_name = "Tuned Hist Gradient Boosting") : 
         # Split the data 
@@ -77,3 +78,70 @@ def exp_hist_gradient_tune(
                 y_train_encoded = tunner_df["y_encoded"],
                 candidates      = 200
             )
+            pipeline = search.best_estimator
+
+        # If Dalex create a explainer 
+        if RUN_DALEX_GLOBAL_EXPLANATIONS or RUN_DALEX_LOCAL_EXPLANATIONS :
+            dx_explainer = dx_create_explainer(
+                pipeline = pipeline,
+                x_train  = model_data["x_train"],
+                y_train  = model_data["y_train"],
+                label    = "Tuned Hist Boost"
+            )
+            feature_names = ["tenure","MonthlyCharges","TotalCharges","value_gap"]
+
+        # =========== Run Dalex Global Explanations =====================
+        if RUN_DALEX_GLOBAL_EXPLANATIONS :
+            gfi = dx_global_importance(
+                dalex_explainer = dx_explainer,
+                features_names = feature_names
+            )
+             # Log Global Explanations Plots
+            mlflow_log_dalex_plot(gfi["Loss_Shuffle_Plot"],"variable_importance.html",  "dalex/global")
+            mlflow_log_dalex_plot(gfi["PDP_Plot"]         ,"partial_dependence.html",   "dalex/global")
+            mlflow_log_dalex_plot(gfi["RCDR_Plot"]        ,"residuals_rcdf.html",       "dalex/global")
+
+        # ========== Run Dalex Local Explanations 
+        if RUN_DALEX_LOCAL_EXPLANATIONS : 
+            lme = dx_local_explanations(
+                dalex_explainer = dx_explainer,
+                pipeline        = pipeline,
+                feature_names   = feature_names,
+                x_train         = model_data["x_train"],
+                y_train         = model_data["y_train"]
+            )
+             # Log Local Explanations Plots 
+            mlflow_log_dalex_plot(lme["bd_plot_1"]  ,"breakdown_churn.html",      "dalex/local")
+            mlflow_log_dalex_plot(lme["bd_plot_2"]  ,"breakdown_no_churn.html",   "dalex/local")
+            mlflow_log_dalex_plot(lme["cp_plot_1"]  ,"ceretis_paribus_churn.html",      "dalex/local")
+            mlflow_log_dalex_plot(lme["cp_plot_2"]  ,"ceretis_paribus_no_churn.html",   "dalex/local")
+        
+        # ========= Log the model ========================
+        if RUN_LOG_MODEL :
+            mlflow.sklearn.log_model(
+                sk_model = pipeline,
+                artifact_path = "model",
+                input_sample = model_data["x_train"].head(5),
+                registered_model_name = "Tuned Ada Boost Model"
+        )
+
+        # ========= Experimental Settings ================
+        exp_params = {
+            "model"           : "Hist Boost",
+            "model_type"      : "Boosted Trees",
+            "model_params"    : "Optuna Tuned",
+            "train_data_size" :  0.7,
+            "test_data_size"  :  0.3,
+            "preprocessing"   : "Yeo + NZV + Scale + One_Hot",
+            "feature_eng"     : "Full"
+        }
+        mlflow.log_params(exp_params)
+        model_params = {
+            "model__max_iter"         : 1,
+            "model__learning_rate"    : 0.2,
+            "model__max_leaf_nodes"   : 150 ,
+            "model__min_samples_leaf" : 1,
+            "model__class_weight"     : "balanced"
+        }
+        mlflow.log_model_params(model_params)
+
