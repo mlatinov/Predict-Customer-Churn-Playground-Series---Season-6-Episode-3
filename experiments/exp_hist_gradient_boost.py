d4ee22e04982d5be6edf30110e0b27047654ab57
mlatinov <latinovmetodi@gmail.com>
2026-04-09 23:30:14 +0300

    add optuna optimization method to tunners and tune hist with it

--- experiments/exp_hist_gradient_boost.py
+++ experiments/exp_hist_gradient_boost.py
@@ -1,9 +1,8 @@
 
 import mlflow 
 from sklearn.ensemble import HistGradientBoostingClassifier
-from scipy.stats import randint, uniform
 from functions.modeling_f.modeling_helpers_f import evaluate_model, confusion_matrix, model_predict
-from functions.modeling_f.tunner_f import tunner_successive_halving
+from functions.modeling_f.tunner_f import opt_tunner , opt_tune_viz , mlflow_log_optuna_plot
 from functions.data_prep_f.features_helpers import build_pipeline
 from functions.data_prep_f.data_helpers import data_split
 from functions.modeling_f.dalex_f import (
@@ -11,7 +10,8 @@ from functions.modeling_f.dalex_f import (
     dx_global_importance,
     dx_local_explanations,
     mlflow_log_dalex_plot,
-    dx_transform
+    dx_transform,
+    dx_residual_analysis
 )
 
 def exp_hist_gradient_tune(
@@ -29,7 +29,13 @@ def exp_hist_gradient_tune(
         )
         # Create the model pipeline 
         pipeline = build_pipeline(
-            model = HistGradientBoostingClassifier(),
+            model = HistGradientBoostingClassifier(
+                learning_rate    = 0.0267,
+                max_iter         = 442,
+                max_leaf_nodes   = 65,
+                min_samples_leaf = 18,
+                class_weight = None
+            ),
             add_tenure           = False,
             add_value_x_service  = False,
             add_full_feature     = True,
@@ -56,29 +62,32 @@ def exp_hist_gradient_tune(
 
         # ========== Run Successive halving Tuning ================
         if RUN_hist_gradient_boost : 
-            # Create paramter grid 
-            hgb_imbalanced = {
-                "model__max_iter"          : randint(100, 500),
-                "model__learning_rate"     : uniform(0.01, 0.1),
-                "model__max_leaf_nodes"    : randint(31, 100),
-                "model__min_samples_leaf"  : randint(5, 20),
-                "model__class_weight"      : ['balanced']
-            }
+
             # Prepare the data 
             tunner_df = dx_transform(
                 pipeline = pipeline,
                 x_train  = model_data["x_train"],
                 y_train  = model_data["y_train"]
             )
-            # Run Successive halving 
-            search = tunner_successive_halving(
-                pipeline        = pipeline,
-                param_grid      = hgb_imbalanced,
-                X_transformed   = model_data["x_train"],
-                y_train_encoded = tunner_df["y_encoded"],
-                candidates      = 200
+            # Run Optuna 
+            search = opt_tunner(
+                model_name = "HistGradientBoostingClassifier",
+                x_train  = tunner_df["dalex_df"],
+                y_train  = tunner_df["y_encoded"],
+                n_trials = 50
             )
-            pipeline = search.best_estimator
+            best_tune_value = search.best_value
+            best_parameters = search.best_params
+
+            # Viz the Tuning process
+            opt_viz = opt_tune_viz(search, hyper_params=["learning_rate","max_iter"])
+
+            # Log The Tuning process to mlflow 
+            mlflow.log_model_params(best_parameters)
+            mlflow.log_metric(best_tune_value)
+            mlflow_log_optuna_plot(opt_viz["cordinate_plot"]      , "parallel_cordinate_plot.html","optuna")
+            mlflow_log_optuna_plot(opt_viz["countour_plot"]       , "countour_plot.html"          ,"optuna")
+            mlflow_log_optuna_plot(opt_viz["parameter_importance"], "hyper_params_importance.html","optuna")
 
         # If Dalex create a explainer 
         if RUN_DALEX_GLOBAL_EXPLANATIONS or RUN_DALEX_LOCAL_EXPLANATIONS :
@@ -89,6 +98,10 @@ def exp_hist_gradient_tune(
                 label    = "Tuned Hist Boost"
             )
             feature_names = ["tenure","MonthlyCharges","TotalCharges","value_gap"]
+
+            # Residual Analysis 
+            residual_analysis = dx_residual_analysis(dx_explainer = dx_explainer)
+            mlflow.log_figure(residual_analysis["fig1"], "residual_distribution.png")
 
         # =========== Run Dalex Global Explanations =====================
         if RUN_DALEX_GLOBAL_EXPLANATIONS :
@@ -137,11 +150,11 @@ def exp_hist_gradient_tune(
         }
         mlflow.log_params(exp_params)
         model_params = {
-            "model__max_iter"         : 1,
-            "model__learning_rate"    : 0.2,
-            "model__max_leaf_nodes"   : 150 ,
-            "model__min_samples_leaf" : 1,
-            "model__class_weight"     : "balanced"
+            "learning_rate"    : 0.0267,
+            "max_iter"         : 442,
+            "max_leaf_nodes"   : 65,
+            "min_samples_leaf" : 18,
+            "class_weight"     : "None"
         }
         mlflow.log_model_params(model_params)
 
--- functions/modeling_f/tunner_f.py
+++ functions/modeling_f/tunner_f.py
@@ -1,6 +1,12 @@
-
+import tempfile
+import os
+import mlflow
+from sklearn.model_selection import cross_val_score
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import RandomizedSearchCV ,HalvingRandomSearchCV
+import optuna
+import optuna.visualization as viz
+from sklearn.ensemble import HistGradientBoostingClassifier
 
 def tunner_random(pipeline, param_grid, X_transformed, y_train_encoded, iters = 50) :
     
@@ -36,3 +42,63 @@ def tunner_successive_halving(pipeline, param_grid, X_transformed, y_train_encod
     # Fit the Successive halving on the Transformed Data 
     search.fit(X_transformed, y_train_encoded)
     return search
+
+def opt_tunner(model_name, x_train, y_train, n_trials) :
+
+    if model_name == "HistGradientBoostingClassifier" :
+
+        def objective(trial, x_train = x_train, y_train = y_train) :
+            # Declare parameters 
+            params = {
+                "max_iter"       : trial.suggest_int("max_iter", 300, 700),
+                "learning_rate"  : trial.suggest_float("learning_rate", 0.01, 0.3, log=True),
+                "max_leaf_nodes" : trial.suggest_int("max_leaf_nodes", 20, 100),
+                "min_samples_leaf": trial.suggest_int("min_samples_leaf", 5, 20)
+            }
+            # Build the model 
+            model = HistGradientBoostingClassifier(**params, random_state=42, warm_start = True)
+
+            # Evaluate with Cross Validation 
+            scores = cross_val_score(
+                estimator = model,
+                X = x_train,
+                y = y_train,
+                cv      = 5,
+                scoring = "roc_auc",
+                n_jobs  = 1
+            )
+            return scores.mean() 
+        study = optuna.create_study(direction="maximize")
+        study.optimize(objective, n_trials = n_trials, show_progress_bar = True)
+
+    return study
+
+def opt_tune_viz(opt_study, hyper_params) :
+
+    # Parameter Importance 
+    imp_params = viz.plot_param_importances(opt_study)
+
+    # Parallel Coordinate Plot
+    parallel_c = viz.plot_parallel_coordinate(opt_study)
+
+    # Contour Plot
+    contour_p = viz.plot_contour(opt_study, params= hyper_params)
+
+    # Combine into dict 
+    results = {
+        "parameter_importance" : imp_params,
+        "cordinate_plot"       : parallel_c,
+        "countour_plot"        : contour_p
+    }
+    return results
+
+def mlflow_log_optuna_plot(optuna_result, filename, artifact_path="optuna"):
+    """
+    Save a Optuna plot as an interactive HTML artifact and log it to MLflow.
+    """
+    fig = optuna_result.plot(show=False)   
+
+    with tempfile.TemporaryDirectory() as tmp:
+        filepath = os.path.join(tmp, filename)
+        fig.write_html(filepath)
+        mlflow.log_artifact(filepath, artifact_path=artifact_path)
